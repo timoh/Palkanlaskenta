7bcde1c59c4f41d784a3f82800cb8502c2cba490
Timo H <timo@dealmachine.net>
2011-08-09 17:37:09 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Palkanlaskenta::Application.config.secret_token = 'c865245be64236120fa0befa971ce8294137ad5fb2463f02380384225e58a12e0ffb135fc010b29d0a70b47a26d5f53c1c4337acbed27b59537d261d08f04798'

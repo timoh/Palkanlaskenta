1a581211e3e94697c8439c0f80656350d34328a6
Timo H <timo@dealmachine.net>
2011-08-24 14:12:26 +0300

    Production to semi-debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,11 @@ Palkanlaskenta::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
+  
+  #config.consider_all_requests_local       = true
+  config.action_view.debug_rjs             = true
+  #config.action_controller.perform_caching = false
+  
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 

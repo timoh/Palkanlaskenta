2e9757340b8ae3ddad33e739ee43318f2043140a
Timo H <timo@dealmachine.net>
2011-08-27 19:50:45 +0300

    Invoice totals

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,5 +1,21 @@
 class InvoicesController < ApplicationController
+  helper_method :calculate_sum_invoicable
+  
   def index
     @invoices = Sale.find_all_by_billed(false)
+    @total = calculate_sum_invoicable
+  end
+  
+  private
+  
+  def calculate_sum_invoicable
+    sum_total = 0
+    @invoices = Sale.find_all_by_billed(false)
+    
+    @invoices.each do |invoice|
+      sum_total = sum_total+invoice.sum
+    end
+    
+    sum_total
   end
 end

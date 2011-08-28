8ea731d2dcbc50e6a421971277bd3d18a9e40b59
Timo H <timo@dealmachine.net>
2011-08-28 20:01:13 +0300

    Refactored salary calculations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -24,36 +24,68 @@ class Employee < ActiveRecord::Base
     return tot_dur
   end
   
-  def total_salary
-    tot_salary = 0
-    tot_sales = 0
-    tot_shifts = 0
-    
-    #calculate all sales for employee
+  def provision_subtotal
+    prov_subtotal = 0
     self.sales.each do |sale|
-      tot_sales = tot_sales+(sale.sum*sale.employment.provision)
+      prov_subtotal = prov_subtotal+(sale.sum*sale.employment.provision) 
     end
     
-    #calculate all wages from shifts, except those that come on top
+    prov_subtotal
+  end
+  
+  def additionals_subtotal
+    addit_subtotal = 0
+    self.additions.each do |addition|
+      addit_subtotal = addit_subtotal+addition.sum
+    end
+    
+    addit_subtotal
+    
+  end
+  
+  def shifts_not_on_top_subtotal
+    snot_subtotal = 0
+    
     self.shifts.each do |shift|
       if (!shift.employment.comes_on_top?)
-        tot_shifts = tot_shifts+(shift.duration*shift.employment.hourly_wage)
+        snot_subtotal = snot_subtotal+(shift.duration*shift.employment.hourly_wage)
       end
     end
     
-    #select which is greater
-    if (tot_shifts >= tot_sales)
-      tot_salary = tot_shifts
-    else
-      tot_salary = tot_sales
-    end
-
-    #calculate wages that come on top of the normal salary
+    snot_subtotal
+  end
+  
+  def shifts_on_top_subtotal
+    sot_subtotal = 0
     self.shifts.each do |shift|
       if (shift.employment.comes_on_top?)
-        tot_salary = tot_salary+(shift.duration*shift.employment.hourly_wage)
+        sot_subtotal = sot_subtotal+(shift.duration*shift.employment.hourly_wage)
       end
     end
+    
+    sot_subtotal
+  end
+  
+  
+  def total_salary
+    tot_salary = 0
+    
+    #calculate provision from sales
+    tot_salary = tot_salary+self.provision_subtotal
+    
+    #calculate all wages from shifts, except those that come on top
+    tot_salary = tot_salary+self.shifts_not_on_top_subtotal
+    
+    #select should base salary or provision be paid
+    if (self.shifts_not_on_top_subtotal >= self.provision_subtotal)
+      tot_salary = self.shifts_not_on_top_subtotal
+    else
+      tot_salary = self.provision_subtotal
+    end
+    
+    #add wages that come on top of the base salary
+    tot_salary = tot_salary+self.shifts_on_top_subtotal
+    tot_salary = tot_salary+self.additionals_subtotal
     
 
     return tot_salary

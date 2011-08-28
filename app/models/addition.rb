edea1459397970a79aa70fefa0f5a68999dc09d6
Timo H <timo@dealmachine.net>
2011-08-28 19:43:20 +0300

    Validations in the right placde

--- app/models/addition.rb
+++ app/models/addition.rb
@@ -1,4 +1,7 @@
 class Addition < ActiveRecord::Base
   belongs_to :employment
   belongs_to :employee
+  
+  validates_presence_of :title, :date, :sum, :employment_id
+  
 end
--- app/models/calculation.rb
+++ app/models/calculation.rb
@@ -1,5 +1,3 @@
 class Calculation #< ActiveRecord::Base
-  validates_presence_of :title, :date, :sum, :employment_id
-  
 
 end
